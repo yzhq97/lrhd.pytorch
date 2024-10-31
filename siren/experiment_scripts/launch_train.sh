0568c556d69a81ff8cde320024a3c01db730116a
zhuoqian <yzhq97@gmail.com>
2024-10-31 17:12:15 +0100

    try overfit with lora

---
+++ siren/experiment_scripts/launch_train.sh
@@ -0,0 +1,4 @@
+python siren/experiment_scripts/train_sdf.py --config-name=overfit_plane_lora_rank_8_fixed_base_weights &
+python siren/experiment_scripts/train_sdf.py --config-name=overfit_plane_lora_rank_16_fixed_base_weights &
+python siren/experiment_scripts/train_sdf.py --config-name=overfit_plane_lora_rank_8_random_base_weights &
+python siren/experiment_scripts/train_sdf.py --config-name=overfit_plane_lora_rank_16_random_base_weights &
